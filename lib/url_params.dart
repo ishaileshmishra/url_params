fc4c2140b6c12da5c096a0eac911296e0b510f00
mshaileshr@gmail.com <Yes@383840>
2020-05-04 17:49:33 +0530

    1.0.2

---
+++ lib/url_params.dart
@@ -0,0 +1,7 @@
+/// You can use this package to add query parameters to the urls
+///
+/// Example :
+/// In any Flutter or Dart project if you need to append the url parameters
+library params;
+
+export 'src/base.dart';
